583e334a2381e2b23cb1860c0c7bcf5357eea220
ManabiDas202318 <manabidas7011@gmail.com>
2024-03-19 08:34:56 +0530

    code chef

---
+++ code chef/course/LBCL05_problems_CLB024.c
@@ -0,0 +1,19 @@
+/*Addition of Variables
+Write a program which does the following
+
+Declare two integer variables a and b
+Assign the value 23 to a and 20 to b
+Output the sum of a and b to the console
+To sum a and b, you can print a + b, like we did with numbers previously.*/
+#include <stdio.h> 
+int main() {
+	// Assign the value 23 to a
+
+    
+    // Assign the value 20 to b
+    printf("%d",23+20);
+
+    
+    // Output the sum of a and b
+    return o ;
+}
